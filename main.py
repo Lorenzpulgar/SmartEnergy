ef65f711902244694cc4b6c7bf33b1cdd0ea375c
Lorenzpulgar <lorenzpulgar@gmail.com>
2021-08-04 00:04:20 -0500

    Todas las graficas funcionando en tiempo real

--- main.py
+++ main.py
@@ -21,21 +21,30 @@ def _datos(cur):
     cur.execute(
         'SELECT fecha1, corriente1, potencia1, consumo1 FROM datos_tiempo_real1 WHERE id = (SELECT MAX(id) FROM datos_tiempo_real1)')
     datos_tiempo_real1 = cur.fetchall()
+    cur.execute(
+        'SELECT fecha2, corriente2, potencia2, consumo2 FROM datos_tiempo_real2 WHERE id = (SELECT MAX(id) FROM datos_tiempo_real2)')
+    datos_tiempo_real2 = cur.fetchall()
 
     json_data = json.dumps({
             'fecha1': datos_tiempo_real1[0][0], 'corriente1': datos_tiempo_real1[0][1], 
             'potencia1': datos_tiempo_real1[0][2], 'consumo1': datos_tiempo_real1[0][3],
+            'fecha2': datos_tiempo_real2[0][0], 'corriente2': datos_tiempo_real2[0][1], 
+            'potencia2': datos_tiempo_real2[0][2], 'consumo2': datos_tiempo_real2[0][3],
+            'fecha_total': time.strftime("%H: %M: %S"),
+            'corriente_total': datos_tiempo_real1[0][1] + datos_tiempo_real2[0][1],
+            'potencia_total': datos_tiempo_real1[0][2] + datos_tiempo_real2[0][2],
+            'consumo_total': datos_tiempo_real1[0][3] + datos_tiempo_real2[0][3],
         })
  
     return f"data:{json_data}\n\n"
 
 @app.route('/datos_monitoreo')
-def datos_monitoreo():
+def datos_monitoreo1():
     cur = mysql.get_db().cursor()
 
     enviar = _datos(cur)
 
-    return Response(stream_with_context(enviar), mimetype='text/event-stream')
+    return Response(stream_with_context(enviar), mimetype='text/event-stream')   
 
 @app.route('/')
 def login():
